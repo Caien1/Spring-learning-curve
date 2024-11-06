be73ec2946d5aa92d07c0443c1f8f4f3f8e5ab52
Caien1 <pulwama01@gmail.com>
2024-11-06 16:30:29 +0530

    Mappings

--- src/main/java/com/example/springSecurity/HelloController.java
+++ src/main/java/com/example/springSecurity/HelloController.java
@@ -1,34 +1,43 @@
 package com.example.springSecurity;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 @RestController
 
 public class HelloController {
-   @GetMapping("/")
-   public String greet(){
-    return "Hello whats up";
-   }
-
-
-   @RestController
-   public class BlueButton 
-   {
-
-    @GetMapping("/b")
-
-    public String greet(HttpServletRequest req, HttpServletResponse res){
-        System.out.println(req.getSession().getId());
-
-
-        return "<h1> what up <h1>";
+    @GetMapping("/")
+    public String greet() {
+        return "Hello whats up";
     }
-   
-    
-   }
+
+    @RestController
+    public class BlueButton {
+
+        @GetMapping("/b")
+
+        public String greet(HttpServletRequest req, HttpServletResponse res) {
+            System.out.println(req.getSession().getId());
+
+            return "<h1> what up <h1>";
+        }
+
+
+    @RestController
+    public class TokenGetter{
+        @GetMapping("/token")
+        public CsrfToken getToken(HttpServletRequest req){
+            return (CsrfToken) req.getAttribute("_csrf");
+
+        }
+
+
+
+
+    }
+    }
 }
