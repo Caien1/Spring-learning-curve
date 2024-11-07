73232f78f9f82b63097b0a1c67d4d51361d7666a
Caien1 <pulwama01@gmail.com>
2024-11-07 18:22:19 +0530

    TRying to get data from db

---
+++ src/main/java/com/example/springSecurity/model/UserPrincipal.java
@@ -0,0 +1,31 @@
+package com.example.springSecurity.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserPrincipal implements UserDetails{
+private Users user;
+public UserPrincipal(Users user){
+    this.user = user;
+}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+    }
+
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPassword'");
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+    }
+    
+}
--- src/main/java/com/example/springSecurity/service/MyUserDetailService.java
+++ src/main/java/com/example/springSecurity/service/MyUserDetailService.java
@@ -6,6 +6,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.example.springSecurity.model.UserPrincipal;
+import com.example.springSecurity.model.Users;
 import com.example.springSecurity.repo.UserRepo;
 
 @Service
@@ -15,7 +17,13 @@ public class MyUserDetailService implements UserDetailsService{
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-       return null;
+
+        Users user = repo.findByUsername(username);
+
+        if(user==null){
+            throw new UsernameNotFoundException("User not found");
+        }
+       return new UserPrincipal(user);
     }
     
 }
